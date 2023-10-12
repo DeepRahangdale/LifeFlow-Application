eba5ecf4b0a0b9cedeac0342ff0dbf6a44c3549e
DeepRahangdale <deeprahangdale2003@gmail.com>
2023-10-13 01:26:09 +0530

    api

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+const baseURL = "https://lifeflow-app.onrender.com/api/v1/";
+
+const API = axios.create({ baseURL });
 
 //call back function
 //Bearer Token A security token with the property that any party in possession of the token (a “bearer”) 
